d7225d9fb916e429e2b8b6bcff4fb585cb44b60d
manuelpc97 <manuelpc97@unitec.edu>
2016-03-20 13:49:26 -0600

    Incluidas las clases en el main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include "ficha.h"
 #include "mariscal.h"
+#include "general.h"
+#include "coronel.h"
+#include "comandante.h"
+#include "capitan.h"
+#include "teniente.h"
+#include "explorador.h"
+#include "minador.h"
+#include "sargento.h"
+#include "bomba.h"
+#include "bandera.h"
+#include "espia.h"
 
 int main(int argc, char*argv[]){
 
